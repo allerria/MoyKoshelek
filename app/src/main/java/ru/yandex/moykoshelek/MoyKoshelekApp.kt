06c0f83da09387208a3562d55892a58399300d4e
allerria <petrov-georgiy@mail.ru>
2018-07-31 17:13:58 +0300

    deleted ./idea dir. added libs in build.gradle.

--- app/src/main/java/ru/yandex/moykoshelek/MoyKoshelekApp.kt
+++ app/src/main/java/ru/yandex/moykoshelek/MoyKoshelekApp.kt
@@ -3,20 +3,24 @@ package ru.yandex.moykoshelek
 import android.app.Application
 import com.androidnetworking.AndroidNetworking
 import com.treebo.internetavailabilitychecker.InternetAvailabilityChecker
+import timber.log.Timber
 
 
 class MoyKoshelekApp : Application() {
+
+    companion object {
+        @get:Synchronized
+        var instance: MoyKoshelekApp? = null
+            private set
+    }
 
     override fun onCreate() {
         super.onCreate()
         AndroidNetworking.initialize(applicationContext)
         InternetAvailabilityChecker.init(this)
         instance = this
-    }
-
-    companion object {
-        @get:Synchronized
-        var instance: MoyKoshelekApp? = null
-            private set
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
